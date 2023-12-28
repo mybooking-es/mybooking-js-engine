93ccbdddf521871f97231269833766c6f548931f
Anabel Muñoz <amvisualdesigner@gmail.com>
2023-12-28 09:42:28 +0100

    Prettier and eslint config changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,13 +15,15 @@ module.exports = {
   },
   rules: {
     'max-len': ['error', 120], // Max length
-    semi: ['error', 'always'], // Always semi-colon
-    //"quotes": ["warn", "double"], // Always single quotes
+    'semi': ['error', 'always'], // Always semi-colon
+    'space-before-function-paren': ['warn', 'always'], // Space before function paren
+    // 'space-in-parens': [1, 'always'], // Space inside parens
+    'quotes': [2, 'avoid-escape', 'single'],
     // RequireJS integration
-    "requirejs/no-invalid-define": 2,
-    "requirejs/no-multiple-define": 2,
-    "requirejs/no-named-define": 0, // Default is 2 but our use of define caused errors
-    "requirejs/no-commonjs-wrapper": 2,
-    "requirejs/no-object-define": 1    
+    'requirejs/no-invalid-define': 2,
+    'requirejs/no-multiple-define': 2,
+    'requirejs/no-named-define': 0, // Default is 2 but our use of define caused errors
+    'requirejs/no-commonjs-wrapper': 2,
+    'requirejs/no-object-define': 1    
   },
 };
