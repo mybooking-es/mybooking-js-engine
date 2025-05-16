b73248a83fdd0f3299ce52af6d4d123f04e77143
Anabel <anabel@mybooking.es>
2025-05-16 15:35:15 +0200

    Fixed error in exact address field in selector

--- src/rent/selector/SelectorRent.js
+++ src/rent/selector/SelectorRent.js
@@ -618,6 +618,7 @@ define('SelectorRent', ['jquery', 'YSDMemoryDataSource', 'YSDRemoteDataSource','
       $(this.selectorModel.return_place_selector).val('other');
       $(this.selectorModel.return_place_other_selector).val($(this.selectorModel.pickup_place_other_selector).val());
       $(this.selectorModel.custom_return_place_selector).val('true');
+      $(this.selectorModel.return_place_group_selector).hide();
       $(this.selectorModel.another_return_place_group_selector).show();        
 
     }
@@ -690,7 +691,7 @@ define('SelectorRent', ['jquery', 'YSDMemoryDataSource', 'YSDRemoteDataSource','
      * Return place custom address close click
      */
     this.customReturnPlaceCloseBtnClick = function() {
-      $(this.selectorModel.return_place_selector).val('');
+      $(this.selectorModel.return_place_selector).val('').attr('disabled', false);
       $(this.selectorModel.return_place_other_selector).val('');
       $(this.selectorModel.custom_return_place_selector).val('false');      
       $(this.selectorModel.return_place_group_selector).show();
@@ -1910,7 +1911,7 @@ define('SelectorRent', ['jquery', 'YSDMemoryDataSource', 'YSDRemoteDataSource','
                         self.selectorController.customPickupPlaceValueChanged();
                       }
                       else {
-                        $(self.selectorModel.return_place_selector).val($(self.selectorModel.pickup_place_selector).val()).attr('disabled', false);
+                        $(self.selectorModel.return_place_selector).val($(self.selectorModel.pickup_place_selector).val());
                         $(self.selectorModel.return_place_other_selector).val('');
                       }
                       // In both cases notify that the return place has changed
